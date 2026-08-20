35ef40c4e9ee62a89cbb3758642293197ef01f46
Pietro Matos <pietromatoscontato@gmail.com>
2026-08-20 17:46:57 -0300

    Interacting with .txt

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
+def linha():
+    print('='*20)
+
 with open("produtos", "r") as arquivo: #Pega a lista de SKUs sem estoque (atualizar sempre que usar)
     produtos_sem_estoque = [linha.strip() for linha in arquivo if linha.strip()]
 
-print(produtos_sem_estoque)
-
 with open("enviados", "r") as arquivo: #Pega a lista do que já foi enviado pra ser comprado
     produtos_enviados = [linha.strip() for linha in arquivo if linha.strip()]
-print(produtos_enviados)
+
+def atualizar_arquivo(produtos):
+    with open("produtos", "w") as arquivo:
+        for produto in produtos:
+            arquivo.write(produto + '\n')
+
+
+produtos_filtrados = []
+produtos_apagados = []
 
 
 
-for i in range (len(produtos_enviados)): #vê se algum item na lista de produtos sem estoque já foi enviada antes
-    if(i + 1 > len(produtos_sem_estoque)):
-        break
-    if produtos_sem_estoque[i] in produtos_enviados:
-        print(f"({produtos_sem_estoque[i]}) - ESTE ITEM JÁ FOI ENVIADO. REMOVENDO DA LISTA PRINCIPAL.")
-        produtos_sem_estoque.remove(produtos_sem_estoque[i])
 
-print(produtos_sem_estoque)
+linha()
+print('VERIFICANDO SE ALGUM ITEM SEM ESTOQUE JÁ FOI ENVIADO PARA REABASTECER:')
+
+for SKU in produtos_sem_estoque: #verifica se o SKU sem estoque já foi enviado pra reabastecer antes.
+    if SKU in produtos_enviados:
+        print(f"({SKU}) - ESTE ITEM JÁ FOI ENVIADO. REMOVENDO DA LISTA PRINCIPAL.")
+        produtos_apagados.append(SKU)
+    else:
+        produtos_filtrados.append(SKU)
+print(f"ITENS REMOVIDOS: {len(produtos_apagados)}")
+linha()
+
+produtos_sem_estoque = produtos_filtrados
+
+atualizar_arquivo(produtos_sem_estoque)
+
+print(f'PRODUTOS SEM ESTOQUE ({len(produtos_sem_estoque)}):\n{produtos_sem_estoque}')
